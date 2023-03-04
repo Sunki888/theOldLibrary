011de73af4b231a886c580f8d099dd88377fe070
Sunki888 <sankata11@abv.bg>
2023-03-04 14:22:05 +0200

    Initial commit

---
+++ theOldLibrary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace theOldLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bookName = Console.ReadLine();
+            int bookCount = 0;
+            bool isBookFound = false;
+            string currentBook = Console.ReadLine();
+            while (currentBook!="No more books")
+            {
+                if(currentBook==bookName)
+                
+                    isBookFound = true;
+                    Console.WriteLine($"You checked {bookCount} books and found it.");
+                
+               break;
+                bookCount += 1;
+                currentBook = Console.ReadLine();
+
+                if (isBookFound != false)
+                {
+                    Console.WriteLine("The  book you search is not here!");
+                    Console.WriteLine($"You checked {bookCount} books.");
+                }
+            }
+          
+
+        }
+    }
+}
